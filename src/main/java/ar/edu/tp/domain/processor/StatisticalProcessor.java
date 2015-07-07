65461437f03cca900989408781209b5869b7edca
ignaciolunaechechuri <lunaechechuri.ignacio@gmail.com>
2015-07-07 10:15:10 -0300

    Correcion PMD Cyclomatic StaticalProccesor

--- src/main/java/ar/edu/tp/domain/processor/StatisticalProcessor.java
+++ src/main/java/ar/edu/tp/domain/processor/StatisticalProcessor.java
@@ -12,31 +12,32 @@ import ar.edu.tp.exception.TravelNotFoundException;
 
 public class StatisticalProcessor {
 
-	private HashMap<Bike,TimeAndQuantityBike> mapBike;
-	private HashMap <Travel,Integer> mapTravel;
+	private HashMap<Bike, TimeAndQuantityBike> mapBike;
+	private HashMap<Travel, Integer> mapTravel;
 	private int valueMaxBike;
 	private int valueMinBike;
 	private float valueMaxTimeUsedBike;
 	private float totalTimeAverage;
 
-	
-
-	public StatisticalProcessor(HashMap<Bike,TimeAndQuantityBike> mapBike,HashMap <Travel,Integer> mapTravel) throws TravelNotFoundException {
-		this.mapBike=mapBike;
-		this.mapTravel=mapTravel;
+	public StatisticalProcessor(HashMap<Bike, TimeAndQuantityBike> mapBike,
+			HashMap<Travel, Integer> mapTravel) throws TravelNotFoundException {
+		this.mapBike = mapBike;
+		this.mapTravel = mapTravel;
 		this.calculateMaxAndMinBikeAndTime();
-		//validateEmptyTravels();//////////ver esto validar que los mapas no esten vacios
+		// validateEmptyTravels();//////////ver esto validar que los mapas no
+		// esten vacios
 	}
-	
-	
+
 	public float getAverageUseTime() {
 		return totalTimeAverage;
 	}
+
 	public List<Bike> getBikesUsedMoreTimes() {
-		List <Bike> listBike=new LinkedList <Bike>();
-		for (Map.Entry<Bike, TimeAndQuantityBike> mapa : this.mapBike.entrySet()) {
-			TimeAndQuantityBike timeAndQuantityBike=mapa.getValue();
-			if (timeAndQuantityBike.getQuantityBike()==this.valueMaxBike)
+		List<Bike> listBike = new LinkedList<Bike>();
+		for (Map.Entry<Bike, TimeAndQuantityBike> mapa : this.mapBike
+				.entrySet()) {
+			TimeAndQuantityBike timeAndQuantityBike = mapa.getValue();
+			if (timeAndQuantityBike.getQuantityBike() == this.valueMaxBike)
 				listBike.add(mapa.getKey());
 		}
 
@@ -44,10 +45,11 @@ public class StatisticalProcessor {
 	}
 
 	public List<Bike> getBikesUsedLessTimes() {
-		List <Bike> listBike=new LinkedList <Bike>();
-		for (Map.Entry<Bike, TimeAndQuantityBike> mapa : this.mapBike.entrySet()) {
-			TimeAndQuantityBike timeAndQuantityBike=mapa.getValue();
-			if (timeAndQuantityBike.getQuantityBike()==this.valueMinBike)
+		List<Bike> listBike = new LinkedList<Bike>();
+		for (Map.Entry<Bike, TimeAndQuantityBike> mapa : this.mapBike
+				.entrySet()) {
+			TimeAndQuantityBike timeAndQuantityBike = mapa.getValue();
+			if (timeAndQuantityBike.getQuantityBike() == this.valueMinBike)
 				listBike.add(mapa.getKey());
 		}
 
@@ -55,27 +57,27 @@ public class StatisticalProcessor {
 	}
 
 	public List<Bike> getBikeLongerUsed() {
-		List <Bike> listBike=new LinkedList <Bike>();
-		for (Map.Entry<Bike, TimeAndQuantityBike> mapa : this.mapBike.entrySet()) {
-			TimeAndQuantityBike timeAndQuantityBike=mapa.getValue();
-			if (timeAndQuantityBike.getTimeUsed()==this.valueMaxTimeUsedBike)
+		List<Bike> listBike = new LinkedList<Bike>();
+		for (Map.Entry<Bike, TimeAndQuantityBike> mapa : this.mapBike
+				.entrySet()) {
+			TimeAndQuantityBike timeAndQuantityBike = mapa.getValue();
+			if (timeAndQuantityBike.getTimeUsed() == this.valueMaxTimeUsedBike)
 				listBike.add(mapa.getKey());
 		}
 
 		return listBike;
 	}
-	
+
 	public float getValueMaxTimeUsedBike() {
 		return valueMaxTimeUsedBike;
 	}
 
-
 	public List<Travel> getTravelMoreDone() {
-		List <Travel> listTravel=new LinkedList <Travel>();
-			int countMaxTravel=this.travelMoreDone();
+		List<Travel> listTravel = new LinkedList<Travel>();
+		int countMaxTravel = this.travelMoreDone();
 		for (Map.Entry<Travel, Integer> e : this.mapTravel.entrySet()) {
-			Travel travel=e.getKey();
-			 if (countMaxTravel==e.getValue()) {
+			Travel travel = e.getKey();
+			if (countMaxTravel == e.getValue()) {
 				listTravel.add(travel);
 			}
 		}
@@ -83,110 +85,70 @@ public class StatisticalProcessor {
 	}
 
 	private int travelMoreDone() {
-int countMaxTravel=0;
-		
+		int countMaxTravel = 0;
 
 		for (Map.Entry<Travel, Integer> e : this.mapTravel.entrySet()) {
-			
-			 if (countMaxTravel<e.getValue()) {
-				countMaxTravel=e.getValue();
+
+			if (countMaxTravel < e.getValue()) {
+				countMaxTravel = e.getValue();
 			}
 		}
 
 		return countMaxTravel;
 	}
 
-	private void calculateMaxAndMinBikeAndTime() {/////////ver bien nombre
+	private void calculateMaxAndMinBikeAndTime() {// ///////ver bien nombre
 		float timeTotal = 0;
 		int quantity = 0;
-		boolean estadoPrimeraVuelta = false;
+		boolean isHeader = false;
 
-		for (Map.Entry<Bike, TimeAndQuantityBike> mapa : this.mapBike.entrySet()) {
-			TimeAndQuantityBike timeAndQuantityBike=mapa.getValue();
-			timeTotal+=timeAndQuantityBike.getTimeUsed();
-			quantity+=timeAndQuantityBike.getQuantityBike();
-			
-			if (!estadoPrimeraVuelta) {
-				
-				this.valueMaxBike = this.valueMinBike=timeAndQuantityBike.getQuantityBike();
-				this.valueMaxTimeUsedBike=timeAndQuantityBike.getTimeUsed();
-				estadoPrimeraVuelta = true;
-				
+		for (Map.Entry<Bike, TimeAndQuantityBike> mapa : this.mapBike
+				.entrySet()) {
+			TimeAndQuantityBike timeAndQuantityBike = mapa.getValue();
+			timeTotal += timeAndQuantityBike.getTimeUsed();
+			quantity += timeAndQuantityBike.getQuantityBike();
 
-				
-			} else {
-				if (this.valueMaxBike < timeAndQuantityBike.getQuantityBike())
-					this.valueMaxBike = timeAndQuantityBike.getQuantityBike();
-
-				if (this.valueMinBike > timeAndQuantityBike.getQuantityBike())
-					this.valueMinBike = timeAndQuantityBike.getQuantityBike();
-				
-				if (this.valueMaxTimeUsedBike<timeAndQuantityBike.getTimeUsed())
-					this.valueMaxTimeUsedBike=timeAndQuantityBike.getTimeUsed();
-			}
+			isHeader=this.allocateVariables(isHeader, timeAndQuantityBike);
 
 		}
-		this.totalTimeAverage=timeTotal / quantity;
+		this.totalTimeAverage = timeTotal / quantity;
 	}
 
-	/*private List<Bike> findBikesByFrecuency(Map<Bike, Integer> useOfBike, Integer frecuency) {
-		List<Bike> bikes = new LinkedList<Bike>();
-		for (Map.Entry<Bike, Integer> e : useOfBike.entrySet()) {
-			Bike key = e.getKey();
-			Integer value = e.getValue();
-			if (value.equals(frecuency)) {
-				bikes.add(key);
-			}
+	private boolean allocateVariables(boolean isHeader,
+			TimeAndQuantityBike timeAndQuantityBike) {
+		if (!isHeader) {
+
+			this.valueMaxBike = this.valueMinBike = timeAndQuantityBike
+					.getQuantityBike();
+			this.valueMaxTimeUsedBike = timeAndQuantityBike.getTimeUsed();
+			isHeader = true;
+
+		} else {
+
+			this.allocateValueMaxBike(timeAndQuantityBike);
+			this.allocateValueMaxTimeUsedBike(timeAndQuantityBike);
+			this.allocateValueMinBike(timeAndQuantityBike);
+
 		}
-		return bikes;
+		return isHeader;
+
 	}
 
-	private Map<Bike, Integer> generateUseOfBike() {
-		List<Bike> bikes = getAllBikes();
-		Map<Bike, Integer> useOfBike = new HashMap<Bike, Integer>();
-		for (Bike bike : bikes) {
-			if (!useOfBike.containsKey(bike)) {
-				int frequency = Collections.frequency(bikes, bike);
-				useOfBike.put(bike, new Integer(frequency));
-			}
-		}
-		return useOfBike;
+	private void allocateValueMaxBike(TimeAndQuantityBike timeAndQuantityBike) {
+		if (this.valueMaxBike < timeAndQuantityBike.getQuantityBike())
+			this.valueMaxBike = timeAndQuantityBike.getQuantityBike();
+
 	}
 
-	private List<Bike> getAllBikes() {
-		List<Bike> bikes = new ArrayList<Bike>();
-		for (Travel travel : travels) {
-			bikes.add(travel.getBike());
-		}
-		return bikes;
+	private void allocateValueMinBike(TimeAndQuantityBike timeAndQuantityBike) {
+		if (this.valueMinBike > timeAndQuantityBike.getQuantityBike())
+			this.valueMinBike = timeAndQuantityBike.getQuantityBike();
+
 	}
 
-	private Map<Travel, Integer> getTravelCountMap() {
-		Map<Travel, Integer> travelCountMap = new HashMap<Travel, Integer>();
-		int count = 0;
-		for (Travel travel : travels) {
-			if (!travelCountMap.containsKey(travel)) {
-				count = getTravelCount(travel);
-				travelCountMap.put(travel, new Integer(count));
-			}
-		}
-		return travelCountMap;
+	private void allocateValueMaxTimeUsedBike(
+			TimeAndQuantityBike timeAndQuantityBike) {
+		if (this.valueMaxTimeUsedBike < timeAndQuantityBike.getTimeUsed())
+			this.valueMaxTimeUsedBike = timeAndQuantityBike.getTimeUsed();
 	}
-
-	private int getTravelCount(Travel travelToCount) {
-		int count = 0;
-		for (Travel travel : travels) {
-			if (travel.equals(travelToCount)) {
-				count++;
-			}
-		}
-		return count;
-	}
-
-	private void validateEmptyTravels() throws TravelNotFoundException {
-		if (this.travels == null || this.travels.isEmpty()) {
-			throw new TravelNotFoundException("No hay recorridos para procesar");
-		}
-	}
-*/
 }
